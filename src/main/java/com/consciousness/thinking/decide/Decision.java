aed5aa00ac178ba529ed610dcf1a852ec839f2bc
Shawn Catoe <scatoe@paypal.com>
2017-11-15 13:49:17 -0600

    updated for the happiness and joy of things.

--- src/main/java/com/consciousness/thinking/decide/Decision.java
+++ src/main/java/com/consciousness/thinking/decide/Decision.java
@@ -1,5 +1,9 @@
 package com.consciousness.thinking.decide;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 /**
  * This class represents a decision that needs to be made.
  *
@@ -7,9 +11,20 @@ package com.consciousness.thinking.decide;
  */
 public abstract class Decision {
 
+    public enum ModelType {
+        DECIDER,
+        SHADOW;
+    }
+
     /** This is the type of strategy which will be used to interpret the {@link Score}s that are returned. */
     private Strategy.Type strategyType;
     //TODO: provide Strategy implementations?
+
+    /** This is a Map of {@link Model}s that we will execute as a part of this decision. */
+    private Map<ModelType, Model> models = new HashMap<ModelType, Model>();
+
+    /** This is how this object gives data to the {@link Model}s. */
+    private DataProvider dataProvider;
 
     /**
      * Returns the {@link Strategy.Type} value for strategy.
@@ -27,5 +42,40 @@ public abstract class Decision {
      */
     protected void setStrategyType(final Strategy.Type strategyType) {
         this.strategyType = strategyType;
+    }
+
+    /**
+     * This method enqueues a {@link Model} to be used in the {@link Strategy.Type}.
+     * @param model The model to be executed.
+     */
+    protected void addDecidingModel(final Model model) {
+        models.put(ModelType.DECIDER, model);
+    }
+
+    /**
+     * This method enqueues a {@link Model} to be executed outside of the scope of the {@link Strategy.Type}.
+     * @param model The model to be executed.
+     */
+    protected void addShadowModel(final Model model) {
+        models.put(ModelType.SHADOW, model);
+    }
+
+    /**
+     * Returns the {@link DataProvider} value for dataProvider.
+     *
+     * @return The value of dataProvider.
+     */
+    protected DataProvider getDataProvider() {
+        return dataProvider;
+    }
+
+    /**
+     * Sets the {@link DataProvider} value for dataProvider.
+     *
+     * @param dataProvider
+     *         The new value.
+     */
+    protected void setDataProvider(final DataProvider dataProvider) {
+        this.dataProvider = dataProvider;
     }
 }
